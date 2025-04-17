e6300b19ea63242f7ffffd1408d6cc511128cc52
h-user-t <124530361+h-user-t@users.noreply.github.com>
2025-04-17 17:26:34 -0400

    updated recipes.dart

--- hunter_todd_final/lib/recipes.dart
+++ hunter_todd_final/lib/recipes.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<Map<String, dynamic>> fetchRecipe() async {
+  final url =
+      Uri.parse('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata');
+  final response = await http.get(url);
+  
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception('Failed to load recipe');
+  }
+}
 
 class RecipeList extends StatelessWidget {
-  const RecipeList({Key? key}) : super(key: key);
+  const RecipeList({super.key});
 
   @override
   Widget build(BuildContext context) {
